ee8c4409a56447a746d8441f4a5435ea73bb1935
Damian Puerta <55324669+CRistianpuerta1125@users.noreply.github.com>
2023-12-05 19:09:47 -0500

    three commit

--- studentsystem/src/main/java/com/damicode/studentsystem/service/StudentService.java
+++ studentsystem/src/main/java/com/damicode/studentsystem/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.damicode.studentsystem.service;
 
+import com.damicode.studentsystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
--- studentsystem/src/main/java/com/damicode/studentsystem/service/StudentServiceImpl.java
+++ studentsystem/src/main/java/com/damicode/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,18 @@
 package com.damicode.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.damicode.studentsystem.model.Student;
+import com.damicode.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
